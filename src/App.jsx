f943e92b4326e3e9337a5c248f4e8d2add4b0457
Amal C P <acp070146@gmail.com>
2023-06-17 20:26:44 +0530

    pages created ,initalization

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css';
+import "./App.css";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Events from "./components/Events/Events";
+import OurTeam from "./components/OurTeam/Team"
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
     <div className="App">
-    
+      <Home />
+      <About />
+      <Events />
+      <OurTeam />
+      <Contact />
     </div>
   );
 }
