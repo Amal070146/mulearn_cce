b20d58895e3e357c2804d49b4f5298bafaf457c1
Amal C P <acp070146@gmail.com>
2023-06-18 01:28:04 +0530

    home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import "./home.css";
-import bg from '../../assets/home/bg-home.png'
+import mu from "../../assets/home/mu.png";
+import space from "../../assets/home/space.png";
+import earth from "../../assets/home/earth.png";
 
 const Home = () => {
   return (
     <div className="home-wrapper">
-   
-      
+      <div className="home-wrapper-div">
+        <div className="home-main">
+          <div className="home-space-img">
+            <img src={space} alt="" />
+          </div>
+          <div className="home-content-wrapper">
+            <img className="mu" src={mu} alt="" />
+            <div>
+              <div className="home-getready">
+                <p>get ready t</p>
+                <img src={earth} alt="" />
+              </div>
+              <div>
+                <p>witness the</p>
+                <p>µverse</p>
+                <p>at Christ College of Engineering!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button>
+          <span class="m_scroll_arrows unu"></span>
+          <span class="m_scroll_arrows doi"></span>
+          <span class="m_scroll_arrows trei"></span>
+          <span class="m_scroll_arrows quad"></span>
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import menulogo from "../../assets/menulogo.png";
 
 const Navbar = () => {
+  const [openmenu, setopenmenu] = useState(false);
+  function openMenu() {
+    setopenmenu(!openmenu);
+  }
   return (
     <div className="navbar-wrapper">
       <div className="navbar-left">
@@ -27,22 +32,29 @@ const Navbar = () => {
         <button>Join Mulearn</button>
       </div>
       <div className="navbar-mobile">
-        <a href="">
-          <p>Home</p>
-        </a>
-        <a href="">
-          <p>About</p>
-        </a>
-        <a href="">
-          <p>Events</p>
-        </a>
-        <a href="">
-          <p>Our Team</p>
-        </a>
-        <a href="">
-          <p>Contact</p>
-        </a>
-        <button>Join Mulearn</button>
+        <button onClick={openMenu}>
+          <img src={menulogo} alt="" />
+        </button>
+        {openmenu && (
+          <div>
+            <a href="">
+              <p>Home</p>
+            </a>
+            <a href="">
+              <p>About</p>
+            </a>
+            <a href="">
+              <p>Events</p>
+            </a>
+            <a href="">
+              <p>Our Team</p>
+            </a>
+            <a href="">
+              <p>Contact</p>
+            </a>
+            <button>Join Mulearn</button>
+          </div>
+        )}
       </div>
     </div>
   );
