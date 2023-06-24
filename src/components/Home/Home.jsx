2f297f51e5a1b5e125bc7aa2e923abba6eb732dd
Amal C P <acp070146@gmail.com>
2023-06-24 11:17:03 +0530

    home page spotlight

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import "./home.css";
 import mu from "../../assets/home/mu.png";
 import space from "../../assets/home/space.png";
+import content from "../../assets/home/GroupRight.png";
 import earth from "../../assets/home/earth.png";
 
 const Home = () => {
   return (
     <div className="home-wrapper">
       <div className="home-wrapper-div">
-        <div className="home-main">
-          <div className="home-space-img">
-            <img src={space} alt="" />
-          </div>
-          <div className="home-content-wrapper">
+        <div>
+          <div className="home-main">
+            <div className="home-space-img">
+              <img src={space} alt="" />
+            </div>
+            {/* <div className="home-content-wrapper">
             <img className="mu" src={mu} alt="" />
             <div>
               <div className="home-getready">
@@ -31,9 +33,13 @@ const Home = () => {
                 <p>at Christ College of Engineering!</p>
               </div>
             </div>
+          </div> */}
+            <div className="home-content-wrapper">
+              <img src={content} alt="" />
+            </div>
           </div>
         </div>
-        <button>
+        <button onClick={{}}>
           <span class="m_scroll_arrows unu"></span>
           <span class="m_scroll_arrows doi"></span>
           <span class="m_scroll_arrows trei"></span>
