bf8cfe3949c466540a58234e363ef808cd68909a
Amal C P <acp070146@gmail.com>
2023-06-24 11:55:13 +0530

    contact-about-team changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import "./Contact.css";
+import astrologer from '../../assets/contact/astrologer.png'
 
 const Contact = () => {
   return (
     <div className="contact-wrapper">
-      <h1>Get In Touch</h1>
+      <h1>Connect With Us</h1>
+      <div className="contact-content-wrapper">
+        <div className="contact-left-div">
+          <div className="contact-left-div-top">
+            <a href="mailto:mulearn@cce.edu.edu.in">mulearn@cce.edu.edu.in</a>
+            <a href="mailto:mulearn@cce.edu.edu.in">mulearn@cce.edu.edu.in</a>
+          </div>
+          <div>
+            <div>
+              <p>100+</p>
+              <p>STUDENTS ENROLLED</p>
+            </div>
+            <div>
+              <p>20+</p>
+              <p>STUDENTS ENROLLED</p>
+            </div>
+            <div>
+              <p>80K+</p>
+              <p>STUDENTS ENROLLED</p>
+            </div>
+          </div>
+        </div>
+        <div className="contact-right-div">
+          <img src={astrologer} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/OurTeam/Team.jsx
+++ src/components/OurTeam/Team.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import './Team.css'
 
 const Team = () => {
   return (
     <div>
       <h1>Our Team</h1>
+      <div className='team-img-wrapper'>
+        <svg
+          className="team-bg-line"
+          viewBox="0 0 2020 120"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0.196289 0C42.863 35.1667 238.296 116.2 438.696 119C689.196 122.5 895.823 39 1016.7 39C1245.7 39 1321.7 119 1649.2 119C1911.2 119 2019.2 45.3333 2019.2 9"
+            stroke="#D4A7FF"
+            stroke-width="55"
+          />
+        </svg>
+        <svg
+          className="team-bg-line"
+          viewBox="0 0 2020 120"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0.196289 0C42.863 35.1667 238.296 116.2 438.696 119C689.196 122.5 895.823 39 1016.7 39C1245.7 39 1321.7 119 1649.2 119C1911.2 119 2019.2 45.3333 2019.2 9"
+            stroke="#D4A7FF"
+            stroke-width="55"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
