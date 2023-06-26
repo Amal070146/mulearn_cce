11a89a679167b6493e3946530dfcde80a971b3b7
Amal C P <acp070146@gmail.com>
2023-06-27 01:56:31 +0530

    contact section done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Contact.css";
-import astrologer from '../../assets/contact/astrologer.png'
+import astrologer from "../../assets/contact/astrologer.png";
 
 const Contact = () => {
   return (
@@ -22,7 +22,7 @@ const Contact = () => {
             <div>
               <p>20+</p>
               <p>
-               LEARNING<br></br>CIRCLES
+                LEARNING<br></br>CIRCLES
               </p>
             </div>
             <div>
@@ -35,6 +35,14 @@ const Contact = () => {
         </div>
         <div className="contact-right-div">
           <img src={astrologer} alt="" />
+        </div>
+      </div>
+      <div className="outer-div-contact-footer">
+        <div className="outer-div-contact-footer-bottom"></div>
+        <div className="inner-div-contact-footer">
+          <div className="planet-image-wrapper-div">
+            <button>Join Mulearn</button>
+          </div>
         </div>
       </div>
     </div>
