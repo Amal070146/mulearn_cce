4de003598bb18e38f21c9b74123a60be85c4b491
Amal C P <acp070146@gmail.com>
2023-07-08 19:22:49 +0530

    for production

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,26 +13,7 @@ const Home = () => {
             <div className="home-space-img">
               <img src={space} alt="" />
             </div>
-            {/* <div className="home-content-wrapper">
-            <img className="mu" src={mu} alt="" />
-            <div>
-              <div className="home-getready">
-                <div style={{ display: "flex", gap: 15 }}>
-                  <p>get ready</p>
-                  <p>t</p>
-                </div>
-                <img src={earth} alt="" />
-              </div>
-              <div className="home-witness">
-                <div>
-                  <p>witness the</p>
-                  <p>µverse</p>
-                </div>
-
-                <p>at Christ College of Engineering!</p>
-              </div>
-            </div>
-          </div> */}
+            
             <div className="home-content-wrapper">
               <img src={content} alt="" />
               
